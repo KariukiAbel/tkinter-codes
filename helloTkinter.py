941039912d51c3f434f2444e95dcd47aad3495d5
KariukiAbel <abelmuriithi@gmail.com>
2020-11-30 18:20:53 +0300

    On a mission to learn all python

---
+++ helloTkinter.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# creating a window object
+root = Tk()
+
+label1 = Label(root, text="Hello World")
+
+# adding label to the window
+label1.pack()
+
+# looping the window until we hit the close button
+root.mainloop()
